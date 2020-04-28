b67bc9c4c8db205ca891514fc31d016766094f38
Samit Koyom <samitkoyom@gmail.com>
2020-04-29 01:34:39 +0700

    update backend chart

--- src/app/pages/backend/dashboard/dashboard.component.ts
+++ src/app/pages/backend/dashboard/dashboard.component.ts
@@ -11,24 +11,49 @@ export class DashboardComponent implements OnInit {
   dataChart:any;
   optionsChart:any;
 
+  typeChart1:any;
+  dataChart1:any;
+  optionsChart1:any;
+
   constructor() { }
 
   ngOnInit(): void {
 
-    this.typeChart = 'pie' // สามารถกำหนดเป็น 'line','bar','radar','pie','doughnut','polarArea','bubble','scatter
+    // Line Chart
+    this.typeChart = 'bar' // สามารถกำหนดเป็น 'line','bar','radar','pie','doughnut','polarArea','bubble','scatter
    
     this.dataChart = {
-      labels: ["January", "February", "March", "April", "May"],
+      labels: ["Jan","Feb","March","April","May","June","July","August","Sep","Oct","Nov","Dec"],
       datasets:[
           {
                 label: "Stock Summary Chart",
-                data: [10, 30, 50, 30, 40],
-                backgroundColor : ['#1abc9c', '#3498db', '#9b59b6', '#bdc3c7', '#f39c12']
+                data: [9,7,42,30,12,10,15,61,19,3,1,9], 
+                backgroundColor : ['#1abc9c', '#3498db', '#9b59b6', '#bdc3c7', '#f39c12','#1abc9c', '#3498db', '#9b59b6', '#bdc3c7', '#f39c12','#1abc9c', '#3498db']
           }
         ]
     }
     
-     this.optionsChart ={
+     this.optionsChart1 ={
+      responsive: true,
+      maintainAspectRatio: false
+    }
+
+    // Pie Chart
+    this.typeChart1 = 'line' // สามารถกำหนดเป็น 'line','bar','radar','pie','doughnut','polarArea','bubble','scatter
+   
+    this.dataChart1 = {
+      labels: ["Jan","Feb","March","April","May","June","July","August","Sep","Oct","Nov","Dec"], // ชื่อของข้อมูลในแนวแกน x
+      datasets: [{ // กำหนดค่าข้อมูลภายในแผนภูมิแบบเส้น
+        label: 'Number of items sold in months',
+        data: [9,7,42,30,12,10,15,61,19,3,1,9], 
+        fill: false,
+        lineTension: 0.2,
+        borderColor: "orange", // สีของเส้น
+        borderWidth: 4
+     }]
+    }
+    
+     this.optionsChart1 ={
       responsive: true,
       maintainAspectRatio: false
     }
