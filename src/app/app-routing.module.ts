fa454e807740a7a7e93f158f3234616c33081fd4
Samit Koyom <samitkoyom@gmail.com>
2020-04-28 12:12:41 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Route สำหรับเรียกหน้า Frontend Layout
+  {
+    path: '',
+    component: FrontendLayoutComponent,
+    children: [
+      {
+        path:'',
+        component: HomeComponent
+      },
+      {
+        path:'about',
+        component:AboutComponent
+      },
+      {
+      path:'contact',
+      component:ContactComponent
+      }
+    ]
+  },
+  // Route สำหรับเรียกหน้า Login/Register
+  {
+    path:'auth',
+    component:AuthLayoutComponent,
+    children:[
+      {
+        path:'login',
+        component: LoginComponent
+      },
+      {
+        path:'register',
+        component:RegisterComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontendLayoutComponent } from './layouts/frontend-layout/frontend-layout.component';
+import { BackendLayoutComponent } from './layouts/backend-layout/backend-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { HeaderFrontComponent } from './shared/frontend/header-front/header-front.component';
+import { FooterFrontComponent } from './shared/frontend/footer-front/footer-front.component';
+import { HomeComponent } from './pages/frontend/home/home.component';
+import { AboutComponent } from './pages/frontend/about/about.component';
+import { ContactComponent } from './pages/frontend/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontendLayoutComponent,
+    BackendLayoutComponent,
+    AuthLayoutComponent,
+    HeaderFrontComponent,
+    FooterFrontComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
